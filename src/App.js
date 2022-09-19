7733b61169e2aea5c885e389384417fedec58fa7
jj Park <jin_jooo@naver.com>
2022-09-20 01:04:37 +0900

    banner.js ing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
+import Banner from './components/Banner';
 
 function App() {
-    return (
-        <div>
-            <Nav />
-        </div>
-    );
+  return (
+    <div>
+      <Nav />
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
