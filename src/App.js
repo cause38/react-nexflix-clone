46657af5a9e8bd077055d6ce19d7ec725f1ca8b4
jj Park <jin_jooo@naver.com>
2022-09-19 23:17:04 +0900

    불필요 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
 import Nav from './components/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+    return (
+        <div>
+            <Nav />
+        </div>
+    );
 }
 
 export default App;
